f3381d9ace283a64667a76261f5596300a0f9f62
EstebanJ <estebanjimenezv04@gmail.com>
2025-10-10 10:38:49 -0500

    segundo Commit

---
+++ matplot1.py
@@ -0,0 +1,7 @@
+#Instalar Matplotlib
+#pip install matplotlib o python -m pip install matplotlib
+import matplotlib.pyplot as mplo
+#Matploit
+#Nos permite hacer graficas 
+productos=['Manzanas','Peras','Naranjas']
+ventas=[50,80,30]
--- pandasjson2.py
+++ pandasjson2.py
@@ -17,3 +17,30 @@ print(df2)
 #se unifica la data para que se muestre en un solo conjunto de datos
 df_final=pd.concat([df1,df2],axis=0,ignore_index=True)
 print(df_final)
+#para validar que informacion de que fila se encuentra duplicada
+print('Validacion data duplicada')
+print(df_final.duplicated())
+#para realizar una sumatoria de duplicados
+print('total de datos duplicados')
+print(df_final.duplicated().sum())
+
+#verficar duplicados por columna se usa el subset=['Nombre columna']
+print(df_final)
+print(df_final.duplicated(subset=["Id"]))
+#validar dos campos a la vez
+print(df_final)
+print(df_final.duplicated(subset=["Id",'Nombre']))
+
+#siempre dejar el data frame original y crear versiones con las modificaciones
+#dataframe sin duplicados
+df_limpio=df_final.drop_duplicates(keep="last")#con keep=last elimina las primerass y amntiene las ultimas
+print(df_final)
+print("Dataset sin duplicados: ")
+print(df_limpio)
+
+#Suma edades
+print(df_final)
+#Convertir Texxto en Numero
+df_final['Edad']=pd.to_numeric(df_final['Edad'], errors='raise')#utilizamos el errors course(coerencia) para no tomar el valor, como que le vale verga, y el raise es para saber en que linea es y uno puede mirar para corregir ese dato
+print(f'Suma edades {df_final['Edad'].sum()}')
+
